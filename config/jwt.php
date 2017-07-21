2cf653882c7f55eb4f273704dfc7ec1fb3065b7e
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2017-07-20 22:05:46 -0700

    move the JWT key to env

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'hADhiRW70nHCBXig9hmMm3a22NfO6pPg'),
+    'secret' => env('JWT_SECRET'),
 
     /*
     |--------------------------------------------------------------------------
