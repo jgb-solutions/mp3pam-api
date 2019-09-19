f554aaf2630b8a405927c9c1bf529b332b15be08
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2019-09-19 01:15:42 -0400

    API errors and public/private graphql endpoints'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,79 +4,71 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use \Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		\Illuminate\Auth\AuthenticationException::class,
-		\Illuminate\Auth\Access\AuthorizationException::class,
-		\Symfony\Component\HttpKernel\Exception\HttpException::class,
-		\Illuminate\Database\Eloquent\ModelNotFoundException::class,
-		\Illuminate\Session\TokenMismatchException::class,
-		\Illuminate\Validation\ValidationException::class,
-	];
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $exception
-	 * @return void
-	 */
-	public function report(Exception $exception)
-	{
-		parent::report($exception);
-	}
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $exception
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
-		if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-			return response()->json(['message' => 'Tokenn ou an ekspire.'], $e->getStatusCode());
-		}
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
 
-		if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-			return response()->json(['message' => 'Tokenn ou an envalid.'], $e->getStatusCode());
-		}
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
 
-		if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenMismatchException) {
-			return response()->json(['message' => 'TokenMismatchException'], $e->getStatusCode());
-		}
+    	if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+				return response()->json(['message' => 'Tokenn ou an ekspire.'], $exception->getStatusCode());
+			}
 
-		if ($e instanceof ModelNotFoundException && $request->ajax()) {
-			return response()->json(['message' => 'Nou pa jwenn sa w mande a.'], 404);
-		}
+			if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+				return response()->json(['message' => 'Tokenn ou an envalid.'], $exception->getStatusCode());
+			}
 
-		return parent::render($request, $e);
-	}
+			if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenMismatchException) {
+				return response()->json(['message' => 'TokenMismatchException'], $exception->getStatusCode());
+			}
 
-	/**
-	 * Convert an authentication exception into an unauthenticated response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Illuminate\Auth\AuthenticationException  $exception
-	 * @return \Illuminate\Http\Response
-	 */
-	protected function unauthenticated($request, AuthenticationException $exception)
-	{
-		if ($request->expectsJson()) {
-			return response()->json(['error' => 'Unauthenticated.'], 401);
-		}
+			if ($exception instanceof ModelNotFoundException && $request->ajax()) {
+				return response()->json(['message' => 'Nou pa jwenn sa w mande a.'], 404);
+			}
 
-		return redirect()->guest(route('admin.getLogin'));
-	}
+   //   	if ($request->expectsJson()) {
+			// 	return response()->json(['error' => 'Unauthenticated.'], 401);
+			// }
+
+     	return parent::render($request, $exception);
+    }
 }
